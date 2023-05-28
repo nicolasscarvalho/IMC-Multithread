513c7c16adb2b617439c98a160853fe5bd63a84e
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-05-28 22:11:26 +0000

    adicionando docstrings no server.py

--- server.py
+++ server.py
@@ -3,18 +3,32 @@ import socket
 import json
 import threading
 
-def processing_data_client(received):
-    # IMC (Indice de Massa Corporal)
-    def generate_imc(dict):
+
+
+
+# process the user data 
+class ProcessingDataClient(received):
+
+    def __init__(self):
+        self.result = received
+        
+        self.result['imc'] = generate_imc(received)
+        self.result['statusImc'] = analyse_imc(received['imc'])
+        self.result['tmb'] = generate_tmb(received)
+        self.result['cal'] = generate_cal(received)
+        self.result["nutrientes"] = generate_nutrients(received)
+
+
+    # calculate the IMC value
+    def generate_imc(self, dict):
         h = dict['altura']
         p = dict['peso']
         return round(float(p / (h * h)), 2)
 
-    # adding the imc to data sent by the user
-    received['imc'] = generate_imc(received)
 
-    # Status IMC
-    def analyse_imc(imc):
+
+    # returns the IMC status
+    def analyse_imc(self, imc):
         if imc > 0 and imc < 18.5:
             status = "Abaixo do Peso!"
         elif imc <= 24.9:
@@ -31,11 +45,10 @@ def processing_data_client(received):
             status = "Valores inválidos"
         return status
 
-    # adding the status of the imc to data sent by the user
-    received['statusImc'] = analyse_imc(received['imc'])
 
-    # TMB (Taxa Metabólica Basal)
-    def generate_tmb(dict):
+
+    # returns the TMB (Taxa Metabólica Basal)
+    def generate_tmb(self, dict):
         sex = dict['sexo']
 
         if sex in 'Mm':
@@ -46,10 +59,10 @@ def processing_data_client(received):
 
         return tmb
 
-    # adding the tmb to data sent by the user
-    received['tmb'] = generate_tmb(received)
 
-    def generate_cal(dict):
+
+    # returns the amount of calories
+    def generate_cal(self, dict):
         if dict['nvlAtiv'] == 1:
             fator_ativ = 1.2
 
@@ -64,20 +77,20 @@ def processing_data_client(received):
 
         return round((dict['tmb'] * fator_ativ), 2)
 
-    # adding the cal to data sent by the user
-    received['cal'] = generate_cal(received)
 
-    def generate_nutrients(dict):
+
+    # returns the amount of nutrients
+    def generate_nutrients(self, dict):
         carb = str(round((dict['cal'] * 0.45), 2))
         prot = str(round((dict['cal'] * 0.3), 2))
         fat = str(round((dict['cal'] * 0.25), 2))
 
         return {"carboidratos": carb, "proteinas": prot, "gorduras": fat}
 
-    # adding the nutrients to data sent by the user
-    received["nutrientes"] = generate_nutrients(received)
-    return received
 
+
+
+# realize the user < --- > server conection
 def handle_client(client_socket, addr):
     print('Conectado a {}'.format(str(addr)))
 
@@ -87,7 +100,7 @@ def handle_client(client_socket, addr):
 
     # server processing
     received = json.loads(received)
-    data = processing_data_client(received)
+    data = ProcessingDataClient(received).result
     print('O resultado do processamento é {}'.format(data))
 
     # serialising
@@ -100,20 +113,27 @@ def handle_client(client_socket, addr):
     # finish a connection
     client_socket.close()
 
+
+
+
 # create a socket object
 print('ECHO SERVER para cálculo do IMC')
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 # get a local machine name
 host = '127.0.0.1'
 port = 8000
 
+
 # bind to the port
 server_socket.bind((host, port))
+
 
 #start listening requests
 server_socket.listen()
 print('Serviço rodando na porta {}.'.format(port))
+
 
 while True:
     # establish a connection
